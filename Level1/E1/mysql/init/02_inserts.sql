53753e209be411d0f1cced096e311201f0126216
crist <cristhianchulca49@gmail.com>
2025-12-09 11:10:05 +0100

    refactor: rename tables acording to best practices

--- Level1/E1/mysql/init/02_inserts.sql
+++ Level1/E1/mysql/init/02_inserts.sql
@@ -10,18 +10,18 @@ VALUES
 ('Rambla de Catalunya', '22', '1', 'D', 'Barcelona', '08007', 'España'),
 ('Passeig de Gràcia', '101', '4', 'A', 'Barcelona', '08008', 'España');
 
-INSERT INTO supplier (supplier_name, address_id, phone, fax, NIF)
+INSERT INTO Supplier (supplier_name, address_id, phone, fax, NIF)
 VALUES
 ('OpticPro', 1, '934567890', '934567891', 'A12345678'),
 ('VisionMax', 2, '933445566', '933445567', 'B87654321');
 
-INSERT INTO brand(brand_name, supplier_id)
+INSERT INTO Brand(brand_name, supplier_id)
 VALUES	
 ('Ray-Ban', 1),
 ('Oakley', 1),
 ('Prada', 2);
 
-INSERT INTO glasses (brand_id, left_prescription, right_prescription, frame_type, frame_color, lens_color, price)
+INSERT INTO Glasses (brand_id, left_prescription, right_prescription, frame_type, frame_color, lens_color, price)
 VAlUES 
 (1, -1.25, -1.00, 'plastic', 'black', 'transparent', 120.50),
 (2, -0.75, -0.50, 'metal', 'silver', 'blue', 200.00),
@@ -35,23 +35,23 @@ VALUES
 ('Carlos Ramírez', 6, '611223344', 'carlos@email.com', '2025-12-03'),
 ('Marta Fernández', 7, '622334455', 'marta@email.com', '2025-12-02');
 
-UPDATE customer SET recommended_by = 1 WHERE customer_id = 2;
-UPDATE customer SET recommended_by = 4 WHERE customer_id = 4;
+UPDATE Customer SET recommended_by = 1 WHERE customer_id = 2;
+UPDATE Customer SET recommended_by = 4 WHERE customer_id = 4;
 
-INSERT INTO employee (employee_name)
+INSERT INTO Employee (employee_name)
 VALUES
 ('María Torres'),
 ('Pablo Ruiz'),
 ('Lucía Gómez');
 
-INSERT INTO sale (customer_id, glasses_sold_id, employee_id, sale_date, total_price)
+INSERT INTO Sale (customer_id, glasses_sold_id, employee_id, sale_date, total_price)
 VALUES
 
 (1, 1, 1, '2024-11-15', 120.50),
 (1, 2, 2, '2025-02-10', 200.00),
 (2, 2, 2, '2024-12-20', 200.00),
 (2, 3, 1, '2025-03-05', 175.00),
-(3, 3, 3, '2024-10-03', 175.00),
+(3, 3, 3, '2025-10-03', 175.00),
 (3, 1, 2, '2025-01-12', 120.50),
 (4, 1, 1, '2025-09-25', 120.50),
 (4, 2, 3, '2024-04-18', 200.00),
